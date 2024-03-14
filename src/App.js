a2f2f27f264da6944efa95a3865e47609d593f88
thepratham05 <thepratham05@gmail.com>
2024-03-14 23:56:26 +0530

    Made the Menu Section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './Components/Main';
 import Nav from './Components/Nav';
+import Menu from './Components/Menu';
 
 function App() {
   return (
     <>
     <Nav/>
     <Main/>
+    <Menu/>
     </>
   );
 }
---
+++ src/recipes.js
@@ -0,0 +1,35 @@
+import bruchetta from './Assets/bruchetta.svg';
+import greek from './Assets/greek salad.jpg';
+import lemonDessert from './Assets/lemon dessert.jpg';
+
+const recipes = [
+    { 
+      id: 1,
+      title: "Greek salad",
+      price: 12.99,
+      image: greek,
+      description:
+        "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style fota cheese, garnished with crunchy garlic and rosemary croutons",
+    },
+    {
+      id: 2,
+      title: "Bruchetta",
+      price: 5.99,
+      image: bruchetta,
+      description:
+        "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    },
+    {
+      id: 3,
+      title: "Lemon Dessert",
+      price: 4.78,
+      image: lemonDessert,
+      description:
+        "This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
